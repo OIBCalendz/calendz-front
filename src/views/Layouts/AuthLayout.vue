69b6bffaac0e921f016d493bf6c523ac76f6dfe3
Clément <japanclems287@gmail.com>
2022-11-07 18:04:20 +0100

    degage les trucs

--- src/views/Layouts/AuthLayout.vue
+++ src/views/Layouts/AuthLayout.vue
@@ -53,22 +53,7 @@
               <span class="nav-link-inner--text">Accueil</span>
             </router-link>
           </li>
-          <li class="nav-item">
-            <a
-              href="https://docs.calendz.app"
-              target="_blank"
-              rel="noopenner noreferrer"
-              class="nav-link">
-              <span class="nav-link-inner--text">Documentation</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <router-link
-              to="/contact"
-              class="nav-link">
-              <span class="nav-link-inner--text">Contact</span>
-            </router-link>
-          </li>
+          <!-- TODO: delete route /contact-->
         </ul>
 
         <hr class="d-lg-none">
@@ -123,47 +108,16 @@
                 rel="noreferrer noopener"
                 class="font-weight-bold ml-1"
                 target="_blank">Alexandre Tuet</a>
-              <br> Développeurs actuels :
-              <a
-                rel="noreferrer noopener"
-                class="font-weight-bold ml-1"
-                target="_blank">Maxime Lemaire</a> &
-              <a
-                href="https://doryanchaigneau.fr/"
-                rel="noreferrer noopener"
-                class="font-weight-bold ml-1"
-                target="_blank">Doryan Chaigneau</a>
             </div>
           </div>
           <div class="col-xl-6">
             <ul class="nav nav-footer justify-content-center justify-content-xl-end">
               <li class="nav-item">
                 <a
-                  :href="`https://github.com/calendz/calendz-front/releases/tag/v${version}`"
-                  rel="noreferrer noopener"
-                  class="nav-link"
-                  target="_blank">v{{ version }}</a>
-              </li>
-              <li class="nav-item">
-                <a
-                  href="https://changelog.calendz.app"
-                  rel="noreferrer noopener"
-                  class="nav-link"
-                  target="_blank">Changelog</a>
-              </li>
-              <li class="nav-item">
-                <a
                   href="https://github.com/calendz"
                   rel="noreferrer noopener"
                   class="nav-link"
                   target="_blank">Github</a>
-              </li>
-              <li class="nav-item">
-                <a
-                  href="https://status.calendz.app"
-                  target="_blank"
-                  rel="noreferrer noopener"
-                  class="nav-link">Status</a>
               </li>
             </ul>
           </div>
